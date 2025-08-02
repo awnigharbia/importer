8097de6fb74863128354ed57ca7f976c71d890f5
Awni Gharbia <anongharbia@gmail.com>
2025-08-03 00:56:29 +0300

    fix build issues

--- src/services/googleDriveDownloader.ts
+++ src/services/googleDriveDownloader.ts
@@ -192,12 +192,12 @@ export class GoogleDriveDownloader {
     return new Promise((resolve, reject) => {
       const writeStream = fs.createWriteStream(filePath);
 
-      stream.on('error', (error) => {
+      stream.on('error', (error: any) => {
         writeStream.destroy();
         reject(error);
       });
 
-      writeStream.on('error', (error) => {
+      writeStream.on('error', (error: any) => {
         reject(error);
       });
 
@@ -217,7 +217,7 @@ export class GoogleDriveDownloader {
         chunks.push(chunk);
       });
 
-      stream.on('error', (error) => {
+      stream.on('error', (error: any) => {
         reject(error);
       });
 
